acdb742a58143635335fc7e68a3583f0ed5ec732
DatPTPD11090 <162497226+DatPTPD11090@users.noreply.github.com>
2024-11-09 14:57:06 +0700

    Lab 2

---
+++ ServerGame106/Data/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ServerGame106.Models;
+
+namespace ServerGame106.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<GameLevel> GameLevels { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<LevelResult> levelResults { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<GameLevel>().HasData(
+                new GameLevel { LevelID = 1, title = "Cấp độ 1" },
+                new GameLevel { LevelID = 2, title = "Cấp độ 2" },
+                new GameLevel { LevelID = 3, title = "Cấp độ 3" }
+                );
+            modelBuilder.Entity<Question>().HasData(
+                new Question
+                {
+                    QuestionId = 1,
+                    ContentQuestion = "Câu hỏi 1",
+                    Answer = "Đáp án 1",
+                    Option1 = "Đáp án 1",
+                    Option2 = "Đáp án 2",
+                    Option3 = "Đáp án 3",
+                    Option4 = "Đáp án 4",
+                    levelId = 1
+                },
+                new Question
+                {
+                    QuestionId = 2,
+                    ContentQuestion = "Câu hỏi 2",
+                    Answer = "Đáp án 2",
+                    Option1 = "Đáp án 1",
+                    Option2 = "Đáp án 2",
+                    Option3 = "Đáp án 3",
+                    Option4 = "Đáp án 4",
+                    levelId = 2
+                }
+                );
+        }
+    }
+}
---
+++ ServerGame106/Migrations/20241107085020_update-database.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ServerGame106.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GameLevels",
+                columns: new[] { "LevelID", "Description", "title" },
+                values: new object[,]
+                {
+                    { 1, null, "Cấp độ 1" },
+                    { 2, null, "Cấp độ 2" },
+                    { 3, null, "Cấp độ 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Questions",
+                columns: new[] { "QuestionId", "Answer", "ContentQuestion", "Option1", "Option2", "Option3", "Option4", "levelId" },
+                values: new object[,]
+                {
+                    { 1, "Đáp án 1", "Câu hỏi 1", "Đáp án 1", "Đáp án 2", "Đáp án 3", "Đáp án 4", 1 },
+                    { 2, "Đáp án 2", "Câu hỏi 2", "Đáp án 1", "Đáp án 2", "Đáp án 3", "Đáp án 4", 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GameLevels",
+                keyColumn: "LevelID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "GameLevels",
+                keyColumn: "LevelID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "GameLevels",
+                keyColumn: "LevelID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Questions",
+                keyColumn: "QuestionId",
+                keyValue: 2);
+        }
+    }
+}
