9b60c93a7e8adcd85bd315aa8ca949954a39a8a8
DatPTPD11090 <162497226+DatPTPD11090@users.noreply.github.com>
2024-11-16 17:11:39 +0700

    Lab 3

--- ServerGame106/Program.cs
+++ ServerGame106/Program.cs
@@ -14,6 +14,11 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectio
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "ServerGame106", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +31,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+    "ServerGame106 v1"));
 
 app.UseRouting();
 
