32e5126edead72eefbf41030bbee577289958c3a
DatPTPD11090 <162497226+DatPTPD11090@users.noreply.github.com>
2024-11-19 16:55:01 +0700

    Bổ sung code lab 4

--- ServerGame106/DTO/CheckOTPDTO.cs
+++ ServerGame106/DTO/CheckOTPDTO.cs
@@ -2,5 +2,8 @@
 {
     public class CheckOTPDTO
     {
+        public string Email {  get; set; }
+        public int OTP { get; set; }
+
     }
 }
--- ServerGame106/Models/APIGameController.cs
+++ ServerGame106/Models/APIGameController.cs
@@ -470,5 +470,100 @@ namespace ServerGame106.Models
                 return BadRequest(_response);
             }
         }
+        [HttpPost("CheckOTP")]
+        public async Task<ActionResult> CheckOTP(CheckOTPDTO checkOTPDTO)
+        {
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(checkOTPDTO.Email);
+                if (user == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Notification = "Không tìn thấy người dùng";
+                    _response.Data = null;
+                    return BadRequest(_response);
+
+                }
+                var stringOTP = Convert.ToInt32(checkOTPDTO.OTP).ToString();
+                if (user.OTP == stringOTP)
+                {
+                    _response.IsSuccess = true;
+                    _response.Notification = "Mã OTP chính xác";
+                    _response.Data = user.Email;
+                    return Ok (_response);
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                    _response.Notification = "Mã OTP không chính xác";
+                    _response.Data = null;
+                    return BadRequest(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Notification = "LỖI";
+                _response.Data = ex.Message;
+                return BadRequest(_response);
+            }
+        }
+        [HttpPost("ResetPassword")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordDTO resetPasswordDTO)
+        {
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(resetPasswordDTO.Email);
+                if (user == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Notification = "Không tìm thấy người dùng";
+                    _response.Data = null;
+                    return BadRequest(_response);
+                }
+                var stringOTP = Convert.ToInt32(resetPasswordDTO.OTP).ToString();
+                if (user.OTP == stringOTP)
+                {
+                    DateTime now = DateTime.Now;
+                    user.OTP = $"{stringOTP}_used_" + now.ToString("yyyy_MM_dd_HH_mm_ss");
+
+                    var passwordHasher = new PasswordHasher<IdentityUser>();
+                    user.PasswordHash = passwordHasher.HashPassword(user, resetPasswordDTO.NewPassword);
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        _response.IsSuccess = true;
+                        _response.Notification = "Đổi mật khẩu thành công";
+                        _response.Data = resetPasswordDTO.Email;
+                        return Ok(_response);
+                    }
+                    else
+                    {
+                        _response.IsSuccess = false;
+                        _response.Notification = "Đổi mật khẩu thất bại";
+                        _response.Data = result.Errors;
+                        return BadRequest(_response);
+                    }
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                    _response.Notification = "Mã OTP không chính xác";
+                    _response.Data = null;
+                    return BadRequest(_response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Notification = "Lỗi";
+                _response.Data = ex.Message;
+                return BadRequest(_response);
+            }
+        }
+
     }
 }
+
