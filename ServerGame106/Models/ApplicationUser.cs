7f174748281992afdf0d07337b44d05ad7b6697a
DatPTPD11090 <162497226+DatPTPD11090@users.noreply.github.com>
2024-12-03 03:39:27 +0700

    Lab 7

--- ServerGame106/Models/ApplicationUser.cs
+++ ServerGame106/Models/ApplicationUser.cs
@@ -5,17 +5,18 @@ using System.Text.Json.Serialization;
 
 namespace ServerGame106.Models
 {
-    public class ApplicationUser:IdentityUser
+    public class ApplicationUser : IdentityUser
     {
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [ForeignKey("Region")]
         public int RegionId { get; set; }
-        public string? Avatar {  get; set; }
+        public string? Avatar { get; set; }
+
         public bool IsDeleted { get; set; } = false;
+
         [JsonIgnore]
         public string OTP { get; set; }
         = DateTimeOffset.Now.ToUnixTimeSeconds().ToString() + "none";
-
     }
 }
--- ServerGame106/Program.cs
+++ ServerGame106/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using ServerGame106.Data;
@@ -9,14 +11,25 @@ using ServerGame106.Service;
 using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.AccessDeniedPath = "/Home/AccessDenied"; options.LoginPath = "/Home/Login";
+    options.LogoutPath = "/Home/Logout";
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.SlidingExpiration = true;
+});
+
+
 
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 builder.Services.AddTransient<IEmailService, EmailService>();
@@ -53,6 +66,9 @@ builder.Services.AddSwaggerGen(c =>
         }
     });
 });
+
+
+/*
 // cấu hình jwt
 builder.Services.AddAuthentication(options =>
 {
@@ -74,14 +90,14 @@ builder.Services.AddAuthentication(options =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
     };
 });
-
+*/
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -92,11 +108,34 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
     "ServerGame106 v1"));
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    CreateRoles(services).Wait();
+}
 app.Run();
+
+
+
+async Task CreateRoles(IServiceProvider serviceProvider)
+{
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    string[] roleNames = { "Admin", "User" };
+    IdentityResult roleResult;
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await roleManager.RoleExistsAsync(roleName);
+        if (!roleExist)
+        {
+            roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
